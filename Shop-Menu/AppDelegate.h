0f38459bce78932c81be946e4ef02c3822090395
jamesdunay@gmail.com <jamesdunay@gmail.com>
2014-09-12 10:30:49 -0700

    Initial Commit

---
+++ Shop-Menu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Shop-Menu
+//
+//  Created by james.dunay on 9/12/14.
+//  Copyright (c) 2014 James.Dunay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Shop-MenuTests/Shop_MenuTests.m
@@ -0,0 +1,34 @@
+//
+//  Shop_MenuTests.m
+//  Shop-MenuTests
+//
+//  Created by james.dunay on 9/12/14.
+//  Copyright (c) 2014 James.Dunay. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Shop_MenuTests : XCTestCase
+
+@end
+
+@implementation Shop_MenuTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
